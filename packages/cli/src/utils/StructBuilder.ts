94ed372f6943fd9db2d3c5bc6c9e42500292c9d5
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-20 15:11:45 +0200

    added utils, enums, interfaces for cli

---
+++ packages/cli/src/utils/StructBuilder.ts
@@ -0,0 +1,43 @@
+import { StructCollection } from '../interfaces/StructCollection'
+import { VarType } from '../enums/VarType'
+import { CollectionType } from '../enums/CollectionType'
+import { Collection } from '../interfaces/Collection'
+
+export class StructBuilder {
+    private _struct: StructCollection
+
+    constructor(struct?: StructCollection) {
+        this._struct = struct ?? {}
+    }
+
+    public addVariable(name: string, type: VarType): StructBuilder {
+        this._struct[name] = type
+        return this
+    }
+
+    public addArray(name: string, type: VarType | Collection, size: number): StructBuilder {
+        this._struct[name] = { type: CollectionType.Array, data: { type, size } }
+        return this
+    }
+
+    public addStruct(name: string, struct: StructCollection): StructBuilder {
+        this._struct[name] = { type: CollectionType.Struct, data: struct }
+        return this
+    }
+
+    public build(): StructCollection {
+        return this._struct
+    }
+
+    static toString(name: string, struct: StructCollection): string {
+        return `export const ${name} = ${JSON.stringify(struct, null, 2)}`
+    }
+
+    static createArray(type: VarType | Collection, size: number): Collection {
+        return { type: CollectionType.Array, data: { type, size } }
+    }
+
+    static createStruct(struct: StructCollection): Collection {
+        return { type: CollectionType.Struct, data: struct }
+    }
+}
