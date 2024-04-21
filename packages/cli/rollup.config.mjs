fd7ff357b359b15ba472d66223984745b8d0dc59
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-21 03:14:51 +0200

    added cli rollup config

--- packages/cli/rollup.config.mjs
+++ packages/cli/rollup.config.mjs
@@ -1,31 +1,47 @@
 // rollup.config.mjs
-import typescript from '@rollup/plugin-typescript'
-import commonjs from '@rollup/plugin-commonjs'
-import multi from '@rollup/plugin-multi-entry'
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import multi from "@rollup/plugin-multi-entry";
 
 export default {
-  input: 'src/**/*.ts',
-  external: ['fs', 'path', 'antlr4'],
+  input: "src/**/*.ts",
+  external: [
+    "fs",
+    "path",
+    "rimraf",
+    "commander",
+    "antlr4",
+    "rollup",
+    "rollup/dist/loadConfigFile.js",
+    "@rollup/plugin-typescript",
+    "@rollup/plugin-commonjs",
+    "@rollup/plugin-multi-entry",
+    "@prettier/sync",
+  ],
   output: [
     {
-      dir: 'dist',
-      format: 'cjs',
+      dir: "dist",
+      format: "cjs",
       preserveModules: true,
-      exports: 'named',
+      exports: "named",
+      banner: "#!/usr/bin/env node",
     },
     {
-      dir: 'dist',
-      format: 'es',
-      entryFileNames: '[name].mjs',
+      dir: "dist",
+      format: "es",
+      entryFileNames: "[name].mjs",
       preserveModules: true,
-      exports: 'named',
-    }
+      exports: "named",
+      banner: "#!/usr/bin/env node",
+    },
   ],
   plugins: [
     typescript(),
+    json(),
     commonjs(),
     multi({
       preserveModules: true,
-    })
-  ]
-}
+    }),
+  ],
+};
