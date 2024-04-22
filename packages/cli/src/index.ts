bdd13dcd6c00580720d16091361a9f23936fd70f
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-22 02:16:01 +0200

    removed schema.ts

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -7,37 +7,37 @@ import { format } from '@prettier/sync'
 
 export function generate(schemaPath: string, logging: boolean = false) {
   // Generate content
-  let contentTs = ''
-  let contentDts = ''
-  let contentJs = ''
-  let contentMjs = ''
+  let contentDefinition = ''
+  let contentCommon = ''
+  let contentModule = ''
   const exportNames = []
   const schema = SchemaReader.read(schemaPath, logging)
   for (const [name, struct] of schema.entries()) {
-    const path = struct.path ? `export const ${name}Path = '${struct.path}'\n\n` : ''
+    const path = struct.path ? `const ${name}Path = '${struct.path}'\n\n` : ''
     const template = StructBuilder.toString(name, struct.template)
     const type = new InterfaceBuilder(name, struct.template).build()
-    if (path !== '') contentDts += `export declare const ${name}Path = "${struct.path}";\n\n`
-    contentDts += `export declare const ${name}: I${name};\n\n${type}\n\n`
-    contentTs += `${path}${template}\n\n${type}\n\n`
-    contentJs += `${path}${template}\n\n`
-    contentMjs += `${path}${template}\n\n`
+    if (path !== '') contentDefinition += `export declare const ${name}Path = "${struct.path}";\n\n`
+    contentDefinition += `export declare const ${name}: I${name};\n\n${type}\n\n`
+    contentCommon += `${path}${template}\n\n`
+    contentModule += `${path}${template}\n\n`
     exportNames.push(name)
     path !== '' && exportNames.push(`${name}Path`)
   }
 
   // Write files
-  writeFileSync(join(__dirname, '../src/schema.ts'), format(contentTs, { parser: 'typescript' }))
-  writeFileSync(join(__dirname, '../dist/schema.d.ts'), format(contentDts, { parser: 'typescript' }))
+  writeFileSync(join(__dirname, '../dist/schema.d.ts'), format(contentDefinition, { parser: 'typescript' }))
   writeFileSync(
     join(__dirname, '../dist/schema.js'),
-    format(`'use strict';\n\n${contentJs}\n\n${exportNames.map((name) => `exports.${name} = ${name};`).join('\n')}`, {
-      parser: 'typescript'
-    })
+    format(
+      `'use strict';\n\n${contentCommon}\n\n${exportNames.map((name) => `exports.${name} = ${name};`).join('\n')}`,
+      {
+        parser: 'typescript'
+      }
+    )
   )
   writeFileSync(
     join(__dirname, '../dist/schema.mjs'),
-    format(`${contentMjs}\n\nexport {${exportNames.join(',')}};`, { parser: 'typescript' })
+    format(`${contentModule}\n\nexport {${exportNames.join(',')}};`, { parser: 'typescript' })
   )
 
   console.log(`Generated ${schema.size} structs.`)
