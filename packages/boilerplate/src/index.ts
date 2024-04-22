57d1d0f1758fb59da8e3fe1253921110f97855af
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-22 02:57:07 +0200

    updated boilerplate

--- packages/boilerplate/src/index.ts
+++ packages/boilerplate/src/index.ts
@@ -1,4 +1,4 @@
 import { MappedBuffer } from '@mapped.js/buffer'
-import { Test, ITest, TestPath } from '@mapped.js/cli/dist/schema'
+import { Test, ITest, TestPath } from '@mapped.js/cli/schema'
 
 export const buffer = new MappedBuffer<ITest>(TestPath, Test)
